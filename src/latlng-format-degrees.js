24a766eee6702b807027384f4aff941946ae739a
Niels Holt <niels@steenbuchholt.dk>
2020-05-27 18:07:49 +0200

    Bug fix

--- src/latlng-format-degrees.js
+++ src/latlng-format-degrees.js
@@ -173,8 +173,8 @@ Set methodes and options for format degrees, minutes, seconds
 
             value = value.toUpperCase().trim();
 
-            //If it contains anything but space 0-9,."'NnSsEeWw minus-sign degree-sign => invalid format
-            if (value.replace(/(\s|\d|,|\.|N|n|S|s|W|w|E|e|\°|'|"|\-)/g, '').length)
+            //If it contains anything but space 0-9,."'NnSsEeWw minus-sign@beginning degree-sign => invalid format
+            if (value.replace(/(\s|\d|,|\.|N|n|S|s|W|w|E|e|\°|'|"|^\-)/g, '').length)
                 return false;
 
             //If it is lat and it contains "W" or "E" or it is long and it contains "N" or "S" => invalid format
